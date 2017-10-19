b2ee58ed98b004a65a40b8a2da1d1011522a6e9f
Liad <liad.elidan@mail.huji.ac.il>
2017-10-19 12:27:57 +0100

    test1

---
+++ src/Project1.java
@@ -0,0 +1,43 @@
+//
+// Name : Liad Elidan, ID : 17316414
+//
+// Software Engineering Development project 1
+//
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree
+{
+    Node root;
+
+    /* Function to find LCA of n1 and n2. The function assumes that both
+       n1 and n2 are present in BST */
+    Node lca(Node node, int n1, int n2)
+    {
+        if (node == null)
+            return null;
+
+        // If both n1 and n2 are smaller than root, then LCA lies in left
+        if (node.data > n1 && node.data > n2)
+            return lca(node.left, n1, n2);
+
+        // If both n1 and n2 are greater than root, then LCA lies in right
+        if (node.data < n1 && node.data < n2)
+            return lca(node.right, n1, n2);
+
+        return node;
+    }
+
+    public static void main(String args[])
+    {
+    }
+}
