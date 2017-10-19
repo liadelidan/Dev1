5e94f061e3ffd555a0d0644c4fc8ceea1e039c92
Liad <liad.elidan@mail.huji.ac.il>
2017-10-19 12:34:33 +0100

    test2

---
+++ test/FirstTest.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+    @Test
+    public void firstTest() {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(20);
+        tree.root.left = new Node(8);
+        tree.root.right = new Node(22);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(12);
+        tree.root.left.right.left = new Node(10);
+        tree.root.left.right.right = new Node(14);
+        int n1 = 10, n2 = 14;
+        Node t = tree.lca(tree.root, n1, n2);
+        System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
+
+        n1 = 14;
+        n2 = 8;
+        t = tree.lca(tree.root, n1, n2);
+        System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
+
+        n1 = 10;
+        n2 = 22;
+        t = tree.lca(tree.root, n1, n2);
+        System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
+
+
+    }
+}
