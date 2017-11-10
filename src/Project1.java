e0bdaf8fccb402bcd7abafee2432271669f4d94d
Liad <liad.elidan@mail.huji.ac.il>
2017-11-10 12:17:14 +0000

    test4

--- src/Project1.java
+++ src/Project1.java
@@ -3,6 +3,13 @@
 //
 // Software Engineering Development Project 1
 //
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
 class Node
 {
     int data;
@@ -13,6 +20,26 @@ class Node
         data = item;
         left = right = null;
     }
+}
+
+class Node2<T> {
+    T data;
+    Set<Node2<T>> adj;
+
+    public Node2(T data) {
+        this.data = data;
+        this.adj = new HashSet<>();
+    }
+
+    public void addEdge(Node2<T> node) {
+        adj.add(node);
+    }
+    public void addEdge (Node2[] nodes) {
+        for (Node2 node : nodes) {
+            adj.add(node);
+        }
+    }
+
 }
 
 class BinaryTree
@@ -42,5 +69,56 @@ class BinaryTree
 
     public static void main(String args[])
     {
+    }
+}
+
+class DirectedAcyclicGraph<T> {
+    Node2<T> root;
+
+    public Node2<T> lowestCommonAncestor(Node2<T> one, Node2<T> two) {
+        return lowestCommonAncestor(root, one, two);
+    }
+
+    public Node2<T> lowestCommonAncestor(Node2<T> node, Node2<T> one, Node2<T> two) {
+        if (node == null || one == null || two == null) {
+            return null;
+        }
+        List<Node2<T>> list1 = dfs(node, one, new ArrayList<>(), new Stack<>());
+        List<Node2<T>> list2 = dfs(node, two, new ArrayList<>(), new Stack<>());
+        if (list1 == null || list2 == null) {
+            return null;
+        }
+        List<Node2<T>> minList;
+        List<Node2<T>> maxList;
+        if (list1.size() <= list2.size()) {
+            minList = list1;
+            maxList = list2;
+        } else {
+            minList = list2;
+            maxList = list1;
+        }
+        Set<Node2<T>> set = new HashSet<>();
+        for (Node2<T> n : minList) {
+            set.add(n);
+        }
+        for (int i = maxList.size() - 1; i >= 0; i--) {
+            if (set.contains(maxList.get(i))) {
+                return maxList.get(i);
+            }
+        }
+        return null;
+    }
+
+    private List<Node2<T>> dfs(Node2<T> node, Node2<T> target, List<Node2<T>> list, Stack<Node2<T>> stack) {
+        stack.push(node);
+        for (Node2<T> n : node.adj) {
+            if (n.equals(target)) {
+                list.addAll(stack);
+                return list;
+            }
+            dfs(n, target, list, stack);
+        }
+        stack.pop();
+        return list;
     }
 }
